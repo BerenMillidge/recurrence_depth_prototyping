8a81867f4977bf2d4b3804eaebe59284c9b2a1f6
Beren <bmillidgework@gmail.com>
2021-01-24 18:29:58 +0000

    fix data paths

--- main.py
+++ main.py
@@ -57,14 +57,14 @@ def train_prednet(logdir,savedir,model='PredNetTied',dataset="cifar10", cls=6, g
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
     if dataset == "cifar100":
-      trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
+      trainset = torchvision.datasets.CIFAR100(root='./cifar100_data', train=True, download=False, transform=transform_train)
       trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchsize, shuffle=True, num_workers=2)
-      testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
+      testset = torchvision.datasets.CIFAR100(root='./cifar100_data', train=False, download=False, transform=transform_test)
       testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
     elif dataset == "cifar10":
-      trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+      trainset = torchvision.datasets.CIFAR10(root='./cifar10_data', train=True, download=False, transform=transform_train)
       trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchsize, shuffle=True, num_workers=2)
-      testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+      testset = torchvision.datasets.CIFAR10(root='./cifar10_data', train=False, download=False, transform=transform_test)
       testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
     else:
       raise ValueError("dataset name: " + str(dataset) + " is not supported. Try cifar10 or cifar100")
